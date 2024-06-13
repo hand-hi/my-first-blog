9ca821de7df863b177316f68badc114962fbaa84
hand-hi <hanhipham15@gmail.com>
2024-06-12 18:14:55 -0700

    add post

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'blog',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
